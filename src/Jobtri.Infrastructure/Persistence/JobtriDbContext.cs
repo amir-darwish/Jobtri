07b7f4bf6c0758cd89ad2fe1a96369226a533211
Mohamed Amir DARWISH <mohamed.amir.darwish@gmail.com>
2026-09-19 23:59:17 +0200

    add first docker to use postgreSQL and start to init DB

---
+++ src/Jobtri.Infrastructure/Persistence/JobtriDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Jobtri.Domain.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Jobtri.Infrastructure.Persistence
+{
+    public sealed class JobtriDbContext : DbContext
+    {
+        public DbSet<Company> Companies
+        {
+            get
+            {
+                return Set<Company>();
+            }
+        }
+
+        public DbSet<CompanySource> CompanySources
+        {
+            get
+            {
+                return Set<CompanySource>();
+            }
+        }
+
+        public DbSet<Job> Jobs
+        {
+            get
+            {
+                return Set<Job>();
+            }
+        }
+
+        public JobtriDbContext(DbContextOptions<JobtriDbContext> options) : base(options)
+        {
+        }
+    }
+}
